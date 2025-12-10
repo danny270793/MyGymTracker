1aca7d2f752ef0613ebd92541a5ecb6aa9836700
Danny Vaca <danny270793@icloud.com>
2025-12-10 15:44:18 -0500

    feat: wrap app with error boundary and add routing in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,36 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
-import { App } from './App.tsx'
 import './i18n'
+import { ErrorBoundary } from './components/technicals/error-boundary.tsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AuthenticatedRoutes } from './components/technicals/authenticated-routes.tsx'
+import { NonAuthenticatedRoutes } from './components/technicals/non-authenticated-routes.tsx'
+import { routes } from './hooks/use-router.tsx'
+import { ErrorPage } from './pages/error-page.tsx'
+import { NotFoundPage } from './pages/not-found-page.tsx'
+import ScrollToTop from './components/technicals/scroll-to-top.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary>
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<AuthenticatedRoutes />}>
+            {routes.filter((route) => route.authenticated).map((route) => (
+              <Route key={route.name} path={route.path} element={route.component} />
+            ))}
+          </Route>
+          <Route path="/" element={<NonAuthenticatedRoutes />}>
+            {routes.filter((route) => !route.authenticated).map((route) => (
+              <Route key={route.name} path={route.path} element={route.component} />
+            ))}
+          </Route>
+          <Route path="/error" element={<ErrorPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
+    </ErrorBoundary>
   </StrictMode>,
 )
