de85d98ea96efbd6cc01501e6c2430ea8171ee59
Danny Vaca <dvaca@technisys.com>
2025-12-10 15:17:04 -0500

    feat: use i18n translation in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-export const App = () => {
+import type { FC } from "react"
+import { useTranslation } from "react-i18next"
+
+export const App: FC = () => {
+  const { t } = useTranslation()
+
   return (
     <>
-      <h1>Hello world</h1>
+      <h1>{t('helloWorld')}</h1>
     </>
   )
 }
