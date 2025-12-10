e74d4ede6e9634b5eb99fa55f109fb0b7ae99a75
Danny Vaca <dvaca@technisys.com>
2025-12-10 15:28:00 -0500

    feat: add non-authenticated routes wrapper component

---
+++ src/components/technicals/non-authenticated-routes.tsx
@@ -0,0 +1,15 @@
+import { useEffect, type FC } from "react"
+import { Outlet, useNavigate } from "react-router-dom"
+
+export const NonAuthenticatedRoutes: FC = () => {
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const token: string | null = localStorage.getItem('token')
+        if (token) {
+            navigate('/')
+        }
+    }, [])
+
+    return <Outlet />
+}
