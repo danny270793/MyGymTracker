81e468b8f191aa2082008834e38ea074ff6cd622
Danny Vaca <dvaca@technisys.com>
2025-12-10 15:52:02 -0500

    style: format non-authenticated-routes.tsx with prettier

--- src/components/technicals/non-authenticated-routes.tsx
+++ src/components/technicals/non-authenticated-routes.tsx
@@ -1,16 +1,16 @@
-import { useEffect, type FC } from "react"
-import { Outlet } from "react-router-dom"
-import { useRouter, type Router } from "../../hooks/use-router"
+import { useEffect, type FC } from 'react'
+import { Outlet } from 'react-router-dom'
+import { useRouter, type Router } from '../../hooks/use-router'
 
 export const NonAuthenticatedRoutes: FC = () => {
-    const router: Router = useRouter()
+  const router: Router = useRouter()
 
-    useEffect(() => {
-        const token: string | null = localStorage.getItem('token')
-        if (token) {
-            router.goToHome()
-        }
-    }, [])
+  useEffect(() => {
+    const token: string | null = localStorage.getItem('token')
+    if (token) {
+      router.goToHome()
+    }
+  }, [])
 
-    return <Outlet />
+  return <Outlet />
 }
