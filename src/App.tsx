07a3f3b7fdfa0af4fafa49c378b322169dd8919e
Danny Vaca <danny270793@icloud.com>
2025-12-10 15:32:56 -0500

    refactor: use routes configuration for dynamic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
 import type { FC } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { HomePage } from "./pages/home"
-import { LoginPage } from "./pages/login"
 import { NotFoundPage } from "./pages/not-found-page"
 import { ErrorPage } from "./pages/error-page"
 import { AuthenticatedRoutes } from "./components/technicals/authenticated-routes"
 import { NonAuthenticatedRoutes } from "./components/technicals/non-authenticated-routes"
+import { routes } from "./hooks/use-router"
 
 export const  App: FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<AuthenticatedRoutes />}>
-          <Route path="/" element={<HomePage />} />
+          {routes.filter((route) => route.authenticated).map((route) => (
+            <Route key={route.name} path={route.path} element={route.component} />
+          ))}
         </Route>
         <Route path="/" element={<NonAuthenticatedRoutes />}>
-        <Route path="/login" element={<LoginPage />} />
+          {routes.filter((route) => !route.authenticated).map((route) => (
+            <Route key={route.name} path={route.path} element={route.component} />
+          ))}
         </Route>
         <Route path="/error" element={<ErrorPage />} />
         <Route path="*" element={<NotFoundPage />} />
