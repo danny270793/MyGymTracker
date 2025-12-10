42a54571c905e888026d1eeef8698063810aeb93
Danny Vaca <danny270793@icloud.com>
2025-12-10 15:28:29 -0500

    feat: configure app routing with react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import type { FC } from "react"
-import { useTranslation } from "react-i18next"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { HomePage } from "./pages/home"
+import { LoginPage } from "./pages/login"
+import { NotFoundPage } from "./pages/not-found-page"
+import { ErrorPage } from "./pages/error-page"
+import { AuthenticatedRoutes } from "./components/technicals/authenticated-routes"
+import { NonAuthenticatedRoutes } from "./components/technicals/non-authenticated-routes"
 
-export const App: FC = () => {
-  const { t } = useTranslation()
-
+export const  App: FC = () => {
   return (
-    <>
-      <h1>{t('helloWorld')}</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AuthenticatedRoutes />}>
+          <Route path="/" element={<HomePage />} />
+        </Route>
+        <Route path="/" element={<NonAuthenticatedRoutes />}>
+        <Route path="/login" element={<LoginPage />} />
+        </Route>
+        <Route path="/error" element={<ErrorPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
