d2f0d5547b7725ce5acc8e678e40f0ecace16b63
Danny Vaca <danny270793@icloud.com>
2025-12-10 18:11:44 -0500

    test: add error boundary component test

---
+++ src/tests/components/technicals/error-boundary.test.tsx
@@ -0,0 +1,32 @@
+import { describe, it, expect } from 'vitest'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { ErrorBoundary } from '../../../components/technicals/error-boundary'
+import { i18n } from '../../../i18n'
+
+describe('ErrorBoundary', () => {
+  it('should render ok', () => {
+    const testText = 'Test'
+    render(<ErrorBoundary>
+        <div>{testText}</div>
+      </ErrorBoundary>,
+    )
+    expect(screen.getByText(new RegExp(testText, 'i'))).toBeInTheDocument()
+  })
+
+  it('should render error', () => {
+    const errorButtonText = 'Throw error'
+    render(<ErrorBoundary>
+        <div>
+          <button onClick={() => {
+            throw new Error('Test')
+          }}>
+            {errorButtonText}
+          </button>
+        </div>
+      </ErrorBoundary>,
+    )
+    fireEvent.click(screen.getByText(errorButtonText))
+    const errorMessage = i18n.t('error', { postProcess: 'capitalize' })
+    expect(screen.getByText(new RegExp(errorMessage, 'i'))).toBeInTheDocument()
+  })
+})
