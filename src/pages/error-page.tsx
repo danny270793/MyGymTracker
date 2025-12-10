2ba7e9d7da339614ffcdada33bbe7e54c0e7c72a
Danny Vaca <dvaca@technisys.com>
2025-12-10 15:53:05 -0500

    style: format error-page.tsx with prettier

--- src/pages/error-page.tsx
+++ src/pages/error-page.tsx
@@ -1,12 +1,12 @@
-import type { FC } from "react"
-import { useTranslation } from "react-i18next"
+import type { FC } from 'react'
+import { useTranslation } from 'react-i18next'
 
 export const ErrorPage: FC = () => {
-    const { t } = useTranslation()
-    
-    return (
-        <div>
-            <h1>{t('error', { postProcess: 'capitalize' })}</h1>
-        </div>
-    )
+  const { t } = useTranslation()
+
+  return (
+    <div>
+      <h1>{t('error', { postProcess: 'capitalize' })}</h1>
+    </div>
+  )
 }
