5751bdfd490419d3a2bbd2ff7986c559c8bdf44e
Danny Vaca <danny270793@icloud.com>
2025-12-11 17:15:58 -0500

    feat: implement chaos monkey utility

---
+++ src/utils/chaos-monkey.ts
@@ -0,0 +1,20 @@
+import { faker } from "./faker"
+
+export const chaosMonkey = {
+  delay: async (milliseconds: number = 3000): Promise<void> => {
+    await new Promise((resolve) => setTimeout(resolve, milliseconds))
+  },
+  error: async (): Promise<void> => {
+    throw new Error('Chaos Monkey is enabled')
+  },
+  doRandomStuffOnDevelopment: async (): Promise<void> => {
+    if (import.meta.env.VITE_CHAOS_MONKEY_ENABLED === 'true' && import.meta.env.DEV) {
+      if (faker.boolean()) {
+        await chaosMonkey.delay(faker.numberBetween(1000, 3000))
+        await chaosMonkey.error()
+      } else {
+        await chaosMonkey.delay()
+      }
+    }
+  },
+}
