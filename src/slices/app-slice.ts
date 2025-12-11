e9dcdf476e545c33c7f229a48d46e57a16600526
Danny Vaca <danny270793@icloud.com>
2025-12-11 15:51:47 -0500

    style: format app slice

--- src/slices/app-slice.ts
+++ src/slices/app-slice.ts
@@ -1,27 +1,29 @@
-import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
-import { useSelector } from 'react-redux';
-import type { RootState } from '.';
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit'
+import { useSelector } from 'react-redux'
+import type { RootState } from '.'
 
 export interface AppState {
-    error: Error | null;
+  error: Error | null
 }
 
 const initialState: AppState = {
-    error: null,
+  error: null,
 }
 
 export const appSlice = createSlice({
-    name: 'auth',
-    initialState,
-    reducers: {
-        errorOcurred: (state: AppState, action: PayloadAction<Error>) => {
-            state.error = action.payload;
-        },
+  name: 'auth',
+  initialState,
+  reducers: {
+    errorOcurred: (state: AppState, action: PayloadAction<Error>) => {
+      state.error = action.payload
     },
+  },
 })
 
 export const appActions = appSlice.actions
 export const appReducer = appSlice.reducer
-export const appSelector = <T extends keyof AppState>(value: T): AppState[T] => {
-    return useSelector((state: RootState) => state.app[value])
+export const appSelector = <T extends keyof AppState>(
+  value: T,
+): AppState[T] => {
+  return useSelector((state: RootState) => state.app[value])
 }
