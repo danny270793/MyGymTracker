f1fc0920e3eb11b3c7ad99e4e8e9dbb812a23dc2
Danny Vaca <dvaca@technisys.com>
2025-12-11 10:24:52 -0500

    refactor: use redux auth state in authenticated routes

--- src/components/technicals/authenticated-routes.tsx
+++ src/components/technicals/authenticated-routes.tsx
@@ -1,16 +1,17 @@
 import { useEffect, type FC } from 'react'
 import { Outlet } from 'react-router-dom'
 import { useRouter, type Router } from '../../hooks/use-router'
+import { authSelector } from '../../slices/auth-slice'
 
 export const AuthenticatedRoutes: FC = () => {
   const router: Router = useRouter()
+  const accessToken: string | null = authSelector('accessToken')
 
   useEffect(() => {
-    const token: string | null = localStorage.getItem('token')
-    if (!token) {
+    if (!accessToken) {
       router.goToLogin()
     }
-  }, [])
+  }, [accessToken])
 
   return <Outlet />
 }
