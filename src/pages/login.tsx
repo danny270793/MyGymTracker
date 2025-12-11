718c381dd78aaf3c044f91d30f33113aaca7ce60
Danny Vaca <dvaca@technisys.com>
2025-12-11 15:42:47 -0500

    feat: display auth error on login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,21 +1,24 @@
-import { useState, type FC } from 'react'
+import { type FC } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useDispatch } from 'react-redux'
-import { authActions } from '../slices/auth-slice'
+import { authActions, authSelector } from '../slices/auth-slice'
 
 export const LoginPage: FC = () => {
   const { t } = useTranslation()
   const dispatch = useDispatch()
-  const [isLoggingIn, setIsLoggingIn] = useState(false)
+
+  const authState = authSelector('state');
+  const isLoggingIn = authState === 'login-requested';
+  const authError = authSelector('error');
 
   const onLoginClicked = async () => {
-    setIsLoggingIn(true)
     dispatch(authActions.loginRequested())
   }
 
   return (
     <div>
       <h1>{t('login', { postProcess: 'capitalize' })}</h1>
+      {authError && <p>{authError.message}</p>}
       <button className={`bg-blue-500 text-white p-2 rounded-md ${isLoggingIn ? 'opacity-50 cursor-not-allowed' : ''}`} onClick={onLoginClicked} disabled={isLoggingIn}>{isLoggingIn ? 'Logging in...' : 'Login'}</button>
     </div>
   )
