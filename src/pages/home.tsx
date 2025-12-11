37a1607667c901895ed76edf1f898ff8dc0332f0
Danny Vaca <dvaca@technisys.com>
2025-12-11 15:42:52 -0500

    feat: display auth error on home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,21 +1,24 @@
-import { useState, type FC } from 'react'
+import { type FC } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useDispatch } from 'react-redux'
-import { authActions } from '../slices/auth-slice'
+import { authActions, authSelector } from '../slices/auth-slice'
 
 export const HomePage: FC = () => {
   const { t } = useTranslation()
   const dispatch = useDispatch()
-  const [isLoggingOut, setIsLoggingOut] = useState(false)
+
+  const authState = authSelector('state');
+  const isLoggingOut = authState === 'logout-requested';
+  const authError = authSelector('error');
 
   const onLogoutClicked = async () => {
-    setIsLoggingOut(true)
     dispatch(authActions.logoutRequested())
   }
 
   return (
     <div>
       <h1>{t('helloWorld', { postProcess: 'capitalize' })}</h1>
+      {authError && <p>{authError.message}</p>}
       <button className={`bg-blue-500 text-white p-2 rounded-md ${isLoggingOut ? 'opacity-50 cursor-not-allowed' : ''}`} onClick={onLogoutClicked} disabled={isLoggingOut}>{isLoggingOut ? 'Logging out...' : 'Logout'}</button>
     </div>
   )
