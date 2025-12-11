bd1f4fab6e2793a7576fa7f0d33a633f22bb976d
Danny Vaca <danny270793@icloud.com>
2025-12-11 15:51:58 -0500

    style: format sagas index

--- src/sagas/index.ts
+++ src/sagas/index.ts
@@ -1,27 +1,31 @@
-import { all, call, put, spawn, type ForkEffect } from "redux-saga/effects";
-import { sessionSagas } from "./auth-sagas";
-import { Logger } from "../utils/logger";
-import { appActions } from "../slices/app-slice";
+import { all, call, put, spawn, type ForkEffect } from 'redux-saga/effects'
+import { sessionSagas } from './auth-sagas'
+import { Logger } from '../utils/logger'
+import { appActions } from '../slices/app-slice'
 
-const logger = new Logger('./src/sagas/index.ts');
+const logger = new Logger('./src/sagas/index.ts')
 
-export const sagas: ForkEffect[] = [
-    ...sessionSagas
-]
+export const sagas: ForkEffect[] = [...sessionSagas]
 
 export const rootSaga = function* () {
-    yield all(
+  yield all(
     sagas.map((saga) =>
-        spawn(function* () {
-          try {
-            yield call(function* () {
-              yield saga
-            })
-          } catch (error) {
-            logger.error('general error on sagas', error)
-            yield put(appActions.errorOcurred(error instanceof Error ? error : new Error('Unknown error running saga')))
-          }
-        }),
-      ),
-    )
-  }
+      spawn(function* () {
+        try {
+          yield call(function* () {
+            yield saga
+          })
+        } catch (error) {
+          logger.error('general error on sagas', error)
+          yield put(
+            appActions.errorOcurred(
+              error instanceof Error
+                ? error
+                : new Error('Unknown error running saga'),
+            ),
+          )
+        }
+      }),
+    ),
+  )
+}
