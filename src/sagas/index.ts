3edc32abde87e2e8bc79eff05b4b94c8ad65f775
Danny Vaca <dvaca@technisys.com>
2025-12-11 15:42:36 -0500

    feat: add global error handling to root saga with spawn

--- src/sagas/index.ts
+++ src/sagas/index.ts
@@ -1,8 +1,27 @@
-import { all } from "redux-saga/effects";
-import { watchAuth } from "./auth-sagas";
+import { all, call, put, spawn, type ForkEffect } from "redux-saga/effects";
+import { sessionSagas } from "./auth-sagas";
+import { Logger } from "../utils/logger";
+import { appActions } from "../slices/app-slice";
 
-export function* rootSaga() {
-    yield all([
-        watchAuth()
-    ])
-}
+const logger = new Logger('./src/sagas/index.ts');
+
+export const sagas: ForkEffect[] = [
+    ...sessionSagas
+]
+
+export const rootSaga = function* () {
+    yield all(
+    sagas.map((saga) =>
+        spawn(function* () {
+          try {
+            yield call(function* () {
+              yield saga
+            })
+          } catch (error) {
+            logger.error('general error on sagas', error)
+            yield put(appActions.errorOcurred(error instanceof Error ? error : new Error('Unknown error running saga')))
+          }
+        }),
+      ),
+    )
+  }
