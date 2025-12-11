4a7bd4da16d72a218f350fecac7d1349c43ec1c9
Danny Vaca <danny270793@icloud.com>
2025-12-11 15:42:41 -0500

    feat: add try-catch error handling to auth sagas

--- src/sagas/auth-sagas.ts
+++ src/sagas/auth-sagas.ts
@@ -1,18 +1,32 @@
 import type { PayloadAction } from "@reduxjs/toolkit";
 import { authActions } from "../slices/auth-slice";
-import { delay, put, takeLatest } from "redux-saga/effects";
+import { delay, put, takeLatest, type ForkEffect } from "redux-saga/effects";
+import { Logger } from "../utils/logger";
+
+const logger = new Logger('./src/sagas/auth-sagas.ts');
 
 function* loginSaga(action: PayloadAction<{ username: string; password: string }>) {
-    yield delay(3000)
-    yield put(authActions.loginSuccess({ accessToken: '123', refreshToken: '456', tokenType: 'Bearer' }))
+    logger.debug('loginSaga started');
+    try {
+        throw new Error('error reaching backend');
+        yield delay(3000)
+        yield put(authActions.loginSuccess({ accessToken: '123', refreshToken: '456', tokenType: 'Bearer' }))
+    } catch (error) {
+        yield put(authActions.loginError(error instanceof Error ? error : new Error('Unknown error running login saga')))
+    }
 }
 
 function* logoutSaga(action: PayloadAction<void>) {
-    yield delay(3000)
-    yield put(authActions.logoutSuccess())
+    logger.debug('logoutSaga started');
+    try {
+        yield delay(3000)
+        yield put(authActions.logoutSuccess())
+    } catch (error) {
+        yield put(authActions.logoutError(error instanceof Error ? error : new Error('Unknown error running logout saga')))
+    }
 }
 
-export function* watchAuth() {
-    yield takeLatest(authActions.loginRequested.type, loginSaga);
-    yield takeLatest(authActions.logoutRequested.type, logoutSaga);
-}
+export const sessionSagas: ForkEffect[] = [
+    takeLatest(authActions.loginRequested.type, loginSaga),
+    takeLatest(authActions.logoutRequested.type, logoutSaga)
+]
