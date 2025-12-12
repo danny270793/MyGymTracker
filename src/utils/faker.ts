4e89f6615d80d6781a62d9c5d6aa30a57fc25e33
Danny Vaca <danny270793@icloud.com>
2025-12-12 09:41:23 -0500

    refactor: update faker utility

--- src/utils/faker.ts
+++ src/utils/faker.ts
@@ -1,4 +1,5 @@
 export const faker = {
-    boolean: () => Math.random() > 0.5,
-    numberBetween: (min: number, max: number) => Math.floor(Math.random() * (max - min + 1)) + min,
+  boolean: () => Math.random() > 0.5,
+  numberBetween: (min: number, max: number) =>
+    Math.floor(Math.random() * (max - min + 1)) + min,
 }
