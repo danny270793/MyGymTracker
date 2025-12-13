78187e09377a2b6edbaeac8d4ea5b25bdbc12387
Danny Vaca <danny270793@icloud.com>
2025-12-12 20:33:57 -0500

    feat: wrap home page with layout component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next'
 import { useDispatch } from 'react-redux'
 import { authActions, authSelector } from '../slices/auth-slice'
 import { useTheme } from '../contexts/use-theme.tsx'
+import { Layout } from '../components/stateless/layout.tsx'
 
 export const HomePage: FC = () => {
   const { t } = useTranslation()
@@ -19,36 +20,45 @@ export const HomePage: FC = () => {
   }
 
   return (
-    <div>
-      <h1>{t('helloWorld', { postProcess: 'capitalize' })}</h1>
-      {hasError && <p>{authError!.message}</p>}
-      <button
-        className={`bg-blue-500 text-white p-2 rounded-md ${isLoggingOut ? 'opacity-50 cursor-not-allowed' : ''}`}
-        onClick={onLogoutClicked}
-        disabled={isLoggingOut}
-      >
-        {isLoggingOut ? 'Logging out...' : 'Logout'}
-      </button>
-      <p>Current theme: {theme}</p>
-      <p>Full theme: {fullTheme}</p>
-      <button
-        className="bg-blue-500 text-white p-2 rounded-md dark:bg-gray-800"
-        onClick={() => setTheme('light')}
-      >
-        Set Light Theme
-      </button>
-      <button
-        className="bg-blue-500 text-white p-2 rounded-md dark:bg-gray-800"
-        onClick={() => setTheme('dark')}
-      >
-        Set Dark Theme
-      </button>
-      <button
-        className="bg-blue-500 text-white p-2 rounded-md dark:bg-gray-800"
-        onClick={() => setTheme('system')}
-      >
-        Set System Theme
-      </button>
-    </div>
+    <Layout>
+      <Layout.Header className="bg-blue-500 dark:bg-gray-800 dark:text-white">
+        <h1>{t('helloWorld', { postProcess: 'capitalize' })}</h1>
+      </Layout.Header>
+      <Layout.Content className="p-4 dark:bg-gray-800 dark:text-white">
+        {hasError && <p>{authError!.message}</p>}
+        <button
+          className={`bg-blue-500 hover:bg-blue-600 text-white
+         dark:bg-gray-600 dark:hover:bg-gray-700
+         px-4 py-2 rounded ${isLoggingOut ? 'opacity-50 cursor-not-allowed' : ''}`}
+          onClick={onLogoutClicked}
+          disabled={isLoggingOut}
+        >
+          {isLoggingOut ? 'Logging out...' : 'Logout'}
+        </button>
+        <p>Current theme: {theme}</p>
+        <p>Full theme: {fullTheme}</p>
+        <button
+          className="bg-blue-500 hover:bg-blue-600 text-white dark:bg-gray-600 dark:hover:bg-gray-700 px-4 py-2 rounded"
+          onClick={() => setTheme('light')}
+        >
+          Set Light Theme
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-600 text-white dark:bg-gray-600 dark:hover:bg-gray-700 px-4 py-2 rounded"
+          onClick={() => setTheme('dark')}
+        >
+          Set Dark Theme
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-600 text-white dark:bg-gray-600 dark:hover:bg-gray-700 px-4 py-2 rounded"
+          onClick={() => setTheme('system')}
+        >
+          Set System Theme
+        </button>
+      </Layout.Content>
+      <Layout.Footer className="bg-blue-500 dark:bg-gray-800 dark:text-white">
+        <h1>Footer</h1>
+      </Layout.Footer>
+    </Layout>
   )
 }
