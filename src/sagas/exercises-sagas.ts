a8a35e602973e53a92dcbbd8f12831fb7a3ec377
Danny Vaca <danny270793@icloud.com>
2025-12-17 10:42:00 -0500

    feat(sagas): add createExerciseSaga

--- src/sagas/exercises-sagas.ts
+++ src/sagas/exercises-sagas.ts
@@ -2,6 +2,7 @@ import { call, put, takeLatest, type ForkEffect } from 'redux-saga/effects'
 import {
   exercisesActions,
   type FetchExercisesPayload,
+  type CreateExercisePayload,
 } from '../slices/exercises-slice'
 import { backend, type Exercise } from '../services/backend'
 import { Logger } from '../utils/logger'
@@ -24,7 +25,24 @@ function* fetchExercisesSaga(action: PayloadAction<FetchExercisesPayload>) {
   }
 }
 
+function* createExerciseSaga(action: PayloadAction<CreateExercisePayload>) {
+  try {
+    logger.debug(`creating exercise: ${action.payload.name}`)
+    const exercise: Exercise = yield call(
+      backend.createExercise,
+      action.payload.name,
+      action.payload.muscleId,
+    )
+    logger.debug(`exercise created: ${exercise.id}`)
+    yield put(exercisesActions.createSuccess(exercise))
+  } catch (error) {
+    logger.error('create exercise error', error)
+    yield put(exercisesActions.createError(error as Error))
+  }
+}
+
 export const exercisesSagas: ForkEffect[] = [
   takeLatest(exercisesActions.fetchRequested.type, fetchExercisesSaga),
+  takeLatest(exercisesActions.createRequested.type, createExerciseSaga),
 ]
 
