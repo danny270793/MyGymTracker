1ab560ff90413ccdc8e5c8b4ba8c2979b68f8037
Danny Vaca <danny270793@icloud.com>
2025-12-17 10:13:43 -0500

    feat(sagas): register exercises sagas in root saga

--- src/sagas/index.ts
+++ src/sagas/index.ts
@@ -1,12 +1,17 @@
 import { all, call, put, spawn, type ForkEffect } from 'redux-saga/effects'
 import { sessionSagas } from './auth-sagas'
 import { musclesSagas } from './muscles-sagas'
+import { exercisesSagas } from './exercises-sagas'
 import { Logger } from '../utils/logger'
 import { navigatorActions } from '../slices/navigator-slice'
 
 const logger = new Logger('./src/sagas/index.ts')
 
-export const sagas: ForkEffect[] = [...sessionSagas, ...musclesSagas]
+export const sagas: ForkEffect[] = [
+  ...sessionSagas,
+  ...musclesSagas,
+  ...exercisesSagas,
+]
 
 export const rootSaga = function* () {
   yield all(
